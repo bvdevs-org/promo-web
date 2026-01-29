8f1bb3dc9d5de29ce1204a93f744fa69907e2d05
ave <veselya95@gmail.com>
2026-01-29 18:55:48 +0100

    Responsive Contact

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -37,7 +37,7 @@ export const ContactForm = () => {
   return (
     <div className="max-w-2xl mx-auto my-12">
       <form onSubmit={handleSubmit} className="space-y-5">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="grid grid-cols-2 gap-5">
           <div>
             <label className={labelStyles}>{t("form.labels.firstName")}</label>
             <input
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,16 +9,20 @@ export const Contact = () => {
   return (
     <>
       <SEO routeKey="contact" />
-      <div className="flex flex-col gap-6 px-50 lg:px-80 pb-20">
+      <div className="flex flex-col gap-6 px-16 lg:px-80 pb-20 w-full max-w-screen-2xl mx-auto">
         <div className="flex flex-col gap-6">
-          <h3 className="font-medium text-6xl">{t("contact.hero.title")}</h3>
+          <h3 className="font-medium text-5xl lg:text-6xl">
+            {t("contact.hero.title")}
+          </h3>
           <p className="text-gray-500">{t("contact.hero.description")}</p>
         </div>
       </div>
-      <div className="px-20 lg:px-50 pb-20">
-        <div className="border border-neutral-400 rounded-3xl grid grid-cols-2 px-10 py-5">
+      <div className="px-6 lg:px-50 pb-20">
+        <div className="border border-neutral-400 rounded-3xl grid grid-cols-1 lg:grid-cols-2 px-10 py-5">
           <div className="flex flex-col gap-5 place-content-center">
-            <h3 className="text-4xl">{t("contact.formSection.title")}</h3>
+            <h3 className="text-3xl lg:text-4xl">
+              {t("contact.formSection.title")}
+            </h3>
             <p className="text-gray-500">
               <Trans i18nKey="contact.formSection.description">
                 Please fill out this form, and we will respond to you within one
